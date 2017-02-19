066254eaabd0b24cddcdb737ed0e16c0405e36f4
Kevin Kriek <kevinkriek@gmail.com>
2017-02-19 11:01:50 +0100

    Change code to apply to the Style guide for Python Code

--- media.py
+++ media.py
@@ -1,17 +1,17 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information"""
-    
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline= movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+
+    def __init__(self, title, storyline, image, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
 
     def show_trailer(self):
         # open the trailer in the browser
         webbrowser.open(self.trailer_youtube_url)
-        
